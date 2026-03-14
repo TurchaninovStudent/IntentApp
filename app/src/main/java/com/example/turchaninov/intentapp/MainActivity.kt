637da8ad12b743bda3116bc590071a147352dfed
Karo4a <t1mati666@yandex.ru>
2026-03-14 14:11:50 +0300

    compile all changes make phone call task

--- app/src/main/java/com/example/turchaninov/intentapp/MainActivity.kt
+++ app/src/main/java/com/example/turchaninov/intentapp/MainActivity.kt
@@ -1,14 +1,21 @@
 package com.example.turchaninov.intentapp
 
+import android.content.Intent
 import android.os.Bundle
-import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -17,8 +24,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.net.toUri
 import com.example.turchaninov.intentapp.ui.theme.IntentAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -37,34 +49,114 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+val phoneNumberPattern = "\\+79\\d{9}".toRegex()
+
 @Composable
-fun TextInput() {
-    var textState by remember { mutableStateOf("") }
-    TextField(
-        value = textState,
-        onValueChange = { newText ->
-            textState = newText
+fun PhoneButton(phone : String, isValidPhoneNumber : Boolean, modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+    Button(
+        onClick = {
+            val intent = Intent(Intent.ACTION_DIAL).apply {
+                data = "tel:$phone".toUri()
+            }
+            context.startActivity(intent)
         },
-        label = { Text("Введите текст") },
+        enabled = isValidPhoneNumber,
+        modifier = modifier,
+    ) {
+        Text(stringResource(R.string.phone_button_text))
+    }
+}
+
+@Composable
+fun TextInput(value : String, onValueChange: (String) -> Unit, modifier: Modifier = Modifier) {
+    TextField(
+        value = value,
+        onValueChange = onValueChange,
+        label = { Text(stringResource(R.string.input_placeholder_text)) },
+        modifier = modifier,
     )
 }
 
 @Composable
-fun MainMenu(modifier: Modifier = Modifier) {
-    Column(
-        verticalArrangement = Arrangement.Center,
+fun ShareTextButton(text : String, modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+    Button(
+        onClick = {
+            val intent = Intent(Intent.ACTION_SEND).apply {
+                type = "text/plain"
+                putExtra(Intent.EXTRA_TEXT, text)
+            }
+            if (text != "") {
+                context.startActivity(
+                    Intent.createChooser(intent, context.getString(R.string.share_through_text))
+                )
+            } else {
+                Toast.makeText(context,
+                    context.getString(R.string.share_empty_text_error), Toast.LENGTH_SHORT)
+                    .show()
+            }
+        },
         modifier = modifier,
     ) {
-        TextInput()
+        Text(stringResource(R.string.share_text))
+    }
+}
+
+@Composable
+fun SecondActivityButton(text : String, modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+
+    Button(
+        modifier = modifier,
+        onClick = {
+            val intent = Intent(context, SecondActivity::class.java).apply {
+                type = "text/plain"
+                putExtra(Intent.EXTRA_TEXT, text)
+            }
+            context.startActivity(intent)
+        }
+    ) {
+        Text(stringResource(R.string.open_second_activity_text))
+    }
+}
+@Composable
+fun MainMenu(modifier: Modifier = Modifier) {
+    var textState by remember { mutableStateOf("") }
+    var isValidPhoneNumber by remember { mutableStateOf(false) }
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.fillMaxSize(),
+    ) {
+        TextInput(textState, { newText ->
+            textState = newText
+            isValidPhoneNumber = phoneNumberPattern.matches(newText)
+        })
+        Spacer(modifier = Modifier.height(16.dp))
+        Column(Modifier.width(IntrinsicSize.Max)) {
+            PhoneButton(
+                textState, isValidPhoneNumber,
+                Modifier.fillMaxWidth(),
+            )
+            ShareTextButton(
+                textState,
+                Modifier.fillMaxWidth(),
+            )
+            SecondActivityButton(
+                textState,
+                Modifier.fillMaxWidth(),
+            )
+        }
+
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MainMenuPreview() {
     IntentAppTheme {
-        MainMenu(
-            modifier = Modifier.fillMaxSize()
-        )
+        MainMenu()
     }
 }
--- app/src/main/java/com/example/turchaninov/intentapp/SecondActivity.kt
+++ app/src/main/java/com/example/turchaninov/intentapp/SecondActivity.kt
@@ -1,27 +1,35 @@
 package com.example.turchaninov.intentapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.turchaninov.intentapp.ui.theme.IntentAppTheme
 
 class SecondActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val extraText: String = intent.getStringExtra(Intent.EXTRA_TEXT) ?: R.string.example_text.toString()
+
         enableEdgeToEdge()
         setContent {
             IntentAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting2(
-                        name = "Android",
+                    GivenText(
+                        text = extraText,
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +39,24 @@ class SecondActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting2(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun GivenText(text: String, modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.fillMaxSize(),
+    ) {
+        Text(
+            text = text,
+            modifier = modifier
+        )
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview2() {
+fun GivenTextPreview() {
     IntentAppTheme {
-        Greeting2("Android")
+        GivenText(stringResource(R.string.example_text))
     }
 }
